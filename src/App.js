947ff5839b28b64ed214fafbfb78e73d8019d586
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-26 15:18:59 +0800

    App.js: Added theme provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import React from 'react'
+
+import { ThemeProvider } from '@mui/material/styles'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 import './App.css'
+
+import theme from './theme'
 
 function App() {
 
   return (
-    <div>
-      <h1>Dating App</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>Dating App</h1>
+      </div>
+    </ThemeProvider>
   )
 }
 
