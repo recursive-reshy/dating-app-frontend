5cff153992f57c7a2571a90b8530d8d03dd7cfd1
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-26 16:26:14 +0800

    Header.jsx: Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { makeStyles } from 'tss-react/mui'
+
+// MUI Core
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+
+// Icons
+import PersonIcon from '@mui/icons-material/Person'
+import ForumIcon from '@mui/icons-material/Forum'
+
+const useStyles = makeStyles()( theme => ( {
+		headerIcon: {
+			padding: theme.spacing(2),
+			color: '#F5E6DA'
+		},
+		headerLogo: {
+			objectFit: 'contain',
+			height: 40,
+			borderRadius: '50%',
+		}
+	} ) 
+)
+
+const Header = () => {
+
+	const { classes } = useStyles()
+
+  return (
+    <AppBar>
+			<Toolbar>
+				<img
+					className={classes.headerLogo}
+					src="app-logo.png"
+					alt="logo"
+				/>
+				<div style={ { flex: 1 } } />
+				<IconButton>
+					<PersonIcon className={classes.headerIcon} fontSize="medium" />
+				</IconButton>
+				<IconButton>
+					<ForumIcon className={classes.headerIcon} fontSize="medium" />
+				</IconButton>
+			</Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
