5a6f8ff526efb058e7c84d947f2727437fe6a163
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-26 14:04:15 +0800

    Clear boiler plate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './App.css'
 
 function App() {
 
   return (
     <div>
+      <h1>Dating App</h1>
     </div>
   )
 }
