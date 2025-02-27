4a730e163ae7556966d0058a311bd559cdf3975f
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-27 13:41:00 +0800

    App.jsx: Mounted DatingCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 
+// MUI Core
 import { ThemeProvider } from '@mui/material/styles'
-
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 
 import './App.css'
 
 import theme from './theme'
 
-import { Header } from './components'
+import { Header, DatingCard } from './components'
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      <h1>Dating App</h1>
+      <div>
+        <Header />
+        <DatingCard />
+      </div>
     </ThemeProvider>
   )
 }
