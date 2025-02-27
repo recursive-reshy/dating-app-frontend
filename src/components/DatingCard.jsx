130cf780059a6958d6d8825cff7049652bcbbee0
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-02-27 19:50:40 +0800

    components\DatingCard.jsx: fetch data from server

--- src/components/DatingCard.jsx
+++ src/components/DatingCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { makeStyles } from 'tss-react/mui'
 
@@ -9,6 +9,8 @@ import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 
 import TinderDatingCard from 'react-tinder-card'
+
+import { getAllPerople } from '../apis/personService'
 
 const useStyles = makeStyles()( theme => ( {
 	container: {
@@ -32,18 +34,21 @@ const DatingCard = () => {
   
 	const { classes } = useStyles()
 
-	const [ people, setPeople ] = useState( [
-		{ name: 'Alice Johnson', imgUrl: 'https://images.unsplash.com/photo-1524255684952-d7185b509571' },
-		{ name: 'James Smith', imgUrl: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e' },
-		{ name: 'Sophie Martin', imgUrl: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d' },
-		{ name: 'Daniel Lee', imgUrl: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde' },
-		{ name: 'Emma Davis', imgUrl: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2' },
-		{ name: 'Liam Anderson', imgUrl: 'https://images.unsplash.com/photo-1599566150163-29194dcaad36' },
-		{ name: 'Olivia White', imgUrl: 'https://images.unsplash.com/photo-1521119989659-a83eee488004' },
-		{ name: 'Noah Wilson', imgUrl: 'https://images.unsplash.com/photo-1532074205216-d0e1f4b87368' },
-		{ name: 'Ava Brown', imgUrl: 'https://images.unsplash.com/photo-1520201163981-8cc95007dd2a' },
-		{ name: 'Ethan Walker', imgUrl: 'https://images.unsplash.com/photo-1517841905240-472988babdf9' }
-	] )
+	const [ people, setPeople ] = useState([])
+
+	useEffect( () => {
+		const fetchPeople = async () => {
+			try {
+				const { data: { people } } = await getAllPerople()
+				setPeople(people)
+			} catch (error) {
+				console.error(`Error while fetching people: ${error}`)
+				
+			}
+		}
+
+		fetchPeople()
+	}, [] )
 
   return (
 		<div>
